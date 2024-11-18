7df961b052e3e3a8dbe3eaf1e13ade2be7a2a091
SheiEthan <ethan.tillier@etu.univ-smb.fr>
2024-11-18 11:39:56 +0100

    Ca affiche

--- Document App/DocumentTableViewController.swift
+++ Document App/DocumentTableViewController.swift
@@ -7,8 +7,32 @@
 
 import UIKit
 
-class DocumentTableViewController: UITableViewController {
+struct DocumentFile {
+    var title: String
+    var size: Int
+    var imageName: String?
+    var url: URL
+    var type: String
+    
+    // Liste statique de documents pour les tests
+    static let testDocuments: [DocumentFile] = [
+        DocumentFile(title: "Document 1", size: 100, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 2", size: 200, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 3", size: 300, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 4", size: 400, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 5", size: 500, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 6", size: 600, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 7", size: 700, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 8", size: 800, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 9", size: 900, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+        DocumentFile(title: "Document 10", size: 1000, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain")
+    ]
+}
 
+class DocumentTableViewController: UITableViewController {
+    
+    var documents = DocumentFile.testDocuments
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,15 +46,43 @@ class DocumentTableViewController: UITableViewController {
 
 
     // MARK: - Table view data source
-
+    
+    // Indique au Controller combien de sections il doit afficher
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
+    // Indique au Controller combien de cellules il doit afficher
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return documents.count
+    }
+    
+    // Indique au Controller comment remplir la cellule avec les données
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Déqueue la cellule réutilisable
+                let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+                
+                // Récupérer le document correspondant à l'index
+                let document = documents[indexPath.row]
+                
+                // Configurer la cellule avec le titre du document
+                cell.textLabel?.text = document.title
+                
+                // Optionnel : Ajouter plus d'informations dans la cellule (ex : taille)
+                cell.detailTextLabel?.text = "Size: \(document.size) KB"
+                
+                // Si tu veux gérer l'affichage d'images, tu peux utiliser `imageName` si disponible
+                if let imageName = document.imageName {
+                    // Utilise l'image associée si l'imageName est défini
+                    cell.imageView?.image = UIImage(named: imageName)
+                } else {
+                    // Si aucune image n'est fournie, tu peux définir une image par défaut
+                    cell.imageView?.image = UIImage(systemName: "doc.text")
+                }
+                
+                return cell
     }
 
     /*
@@ -87,25 +139,4 @@ class DocumentTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    struct DocumentFile {
-        var title: String
-        var size: Int
-        var imageName: String? = nil
-        var url: URL
-        var type: String
-    }
-    
-    static let testDocuments: [DocumentFile] = [
-        DocumentFile(title: "Document 1", size: 100, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 2", size: 200, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 3", size: 300, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 4", size: 400, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 5", size: 500, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 6", size: 600, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 7", size: 700, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 8", size: 800, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 9", size: 900, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
-        DocumentFile(title: "Document 10", size: 1000, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain")
-    ]
-
 }
